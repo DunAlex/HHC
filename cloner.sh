ef864847751246c831628961b6bfc520d6925eed
DunAlex <aleksandun@gmail.com>
2015-01-11 12:04:57 +0200

    cloner upd

--- cloner.sh
+++ cloner.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-rm -R -f membertek-api
-rm -R -f healthyhomecompany-ee
-rm -R -f hhccore
-rm -R -f healthyhomecompany-web
-rm -R -f api
-git clone  https://DunAlex:1hfgye%hegs1@github.com/WattsAssociates/membertek-api.git
-git clone  https://DunAlex:1hfgye%hegs1@github.com/WattsAssociates/healthyhomecompany-ee.git
-git clone  https://DunAlex:1hfgye%hegs1@github.com/WattsAssociates/hhccore.git
-git clone  https://DunAlex:1hfgye%hegs1@github.com/WattsAssociates/healthyhomecompany-web.git
-git clone  https://DunAlex:1hfgye%hegs1@github.com/WattsAssociates/api.git
-cp setup/* healthyhomecompany-ee/system/expressionengine/config
-
+/home/dev29/Dropbox/HealthyHomeCompany/cloner.sh
